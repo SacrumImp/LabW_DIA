cbf435663950dbca163c47b23118eb3bf5c58dd4
VladislavAlpeev <vlad.alpeev@mail.ru>
2020-09-16 15:36:37 +0300

    добавлен внеший пакет

--- Lab2/main.py
+++ Lab2/main.py
@@ -2,13 +2,15 @@ from lab_python_oop.rectangle import Rectangle
 from lab_python_oop.circle import Circle
 from lab_python_oop.square import Square
 
+from colorama import Fore
+
 def main():
     rectangle = Rectangle(1, 1, "синий")
-    print(rectangle)
+    print(Fore.BLUE + str(rectangle))
     circle = Circle(1, "зелёный")
-    print(circle)
+    print(Fore.GREEN + str(circle))
     square = Square(1, "красный")
-    print(square)
+    print(Fore.RED + str(square))
 
 if __name__ == "__main__":
     main()
