37fba3c003a8ac8c7479519a7de764cdce67bdc2
VladislavAlpeev <vlad.alpeev@mail.ru>
2021-01-12 22:08:52 +0300

    добавлен шаблон для заметки в списке

---
+++ HW/rip-hw/src/components/Cards/Card.js
@@ -0,0 +1,16 @@
+import './Card.css'
+
+function Card() {
+    return(
+        <div class="card">
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">Location</p>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+        </div>
+    )
+}
+
+export default Card
