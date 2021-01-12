bace69976dc2f4fc3b83d0231d5a558b9d588b84
VladislavAlpeev <vlad.alpeev@mail.ru>
2021-01-12 22:07:37 +0300

    добавлен контент для главной страницы

---
+++ HW/rip-hw/src/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import logo from '../logo.png';
+import './MainPage.css'
+
+function MainPage(){
+    return(
+        <body className="App-body">
+              <img src={logo} className="App-logo" alt="logo" />
+        </body>
+    )
+}
+
+export default MainPage
