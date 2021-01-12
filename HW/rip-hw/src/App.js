43156231541839d9bf03b44a038c3911c08366e4
VladislavAlpeev <vlad.alpeev@mail.ru>
2021-01-12 22:05:58 +0300

    настроен корень приложения

--- HW/rip-hw/src/App.js
+++ HW/rip-hw/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Header from './Header/Header'
+import MainPage from './MainPage/MainPage'
+import ListPage from './ListPage/ListPage'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router basename="/">
+        <Header />
+        <Switch>
+          <Route path = "/list">
+            <ListPage />
+          </Route>
+          <Route path = "/">
+            <MainPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
