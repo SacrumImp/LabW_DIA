6a71359d40e8dd3a410a540ed53e462e9ea0eb40
VladislavAlpeev <vlad.alpeev@mail.ru>
2021-01-12 22:08:14 +0300

    добавлена страница для списка заметок

---
+++ HW/rip-hw/src/ListPage/ListPage.js
@@ -0,0 +1,13 @@
+import "./ListPage.css"
+
+import Card from '../components/Cards/Card'
+
+function ListPage(){
+    return(
+        <body className="list-body">
+            <Card />
+        </body>
+    )
+}
+
+export default ListPage
