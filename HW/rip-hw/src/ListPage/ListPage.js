5f0622492d17683d22a4f3fd5450b30c7ddae908
VladislavAlpeev <vlad.alpeev@mail.ru>
2021-01-13 00:51:48 +0300

    добавление тестового приложения бекэнда

--- HW/rip-hw/src/ListPage/ListPage.js
+++ HW/rip-hw/src/ListPage/ListPage.js
@@ -1,13 +1,33 @@
+import {Component} from "react"
 import "./ListPage.css"
 
 import Card from '../components/Cards/Card'
 
-function ListPage(){
-    return(
-        <body className="list-body">
-            <Card />
-        </body>
-    )
+class ListPage extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI(){
+        fetch("http://localhost:9000/")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }))
+        .catch(err => err);
+    }
+
+    componentDidMount(){
+        this.callAPI()
+    }
+    
+    render() {
+        return(
+            <body className="list-body">
+                <Card title={this.state.apiResponse}/>
+            </body>
+        )
+    }
 }
 
 export default ListPage
--- HW/rip-hw/src/components/Cards/Card.js
+++ HW/rip-hw/src/components/Cards/Card.js
@@ -1,12 +1,12 @@
 import './Card.css'
 
-function Card() {
+function Card(props) {
     return(
         <div class="card">
                 <img src="..." class="card-img-top" alt="..."/>
                 <div class="card-body">
                     <p class="card-text">Location</p>
-                    <h5 class="card-title">Card title</h5>
+                    <h5 class="card-title">{props.title}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
         </div>
