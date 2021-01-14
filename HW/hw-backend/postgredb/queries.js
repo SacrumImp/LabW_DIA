414a4f0f32dfc83b8672dd26b5b977e2e6eaf9ec
VladislavAlpeev <vlad.alpeev@mail.ru>
2021-01-14 15:01:07 +0300

    добавлены запросы для выборки по id

--- HW/hw-backend/postgredb/queries.js
+++ HW/hw-backend/postgredb/queries.js
@@ -28,7 +28,29 @@ const getImages = (request, response) => {
   })
 }
 
+const getNoteById = (request, response) => {
+  const id = parseInt(request.params.id)
+  pool.query('SELECT * FROM notes WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getImagesById = (request, response) => {
+  const id = parseInt(request.params.id)
+  pool.query('SELECT c.ID as ID, c.id_note as note, i.image as image FROM connections as c LEFT JOIN images as i ON c.id_image = i.ID WHERE c.id_note = $1', [id], (error,results) => {
+    if(error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 module.exports = {
     getNotes,
     getImages,
+    getNoteById,
+    getImagesById,
 }
--- HW/hw-backend/routes/index.js
+++ HW/hw-backend/routes/index.js
@@ -6,5 +6,7 @@ const db = require('../postgredb/queries')
 /* GET home page. */
 router.get('/', db.getNotes);
 router.get('/images', db.getImages);
+router.get('/list/:id', db.getNoteById)
+router.get('/images/:id', db.getImagesById)
 
 module.exports = router;
